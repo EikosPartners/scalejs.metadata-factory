8cdac62248e54b2aeadd33c9ae87274d90600711
jonahmania <jmania999@gmail.com>
2015-09-29 13:17:29 -0400

    remove node_modules and add dstribution files

--- rjsconfig.js
+++ rjsconfig.js
@@ -2,9 +2,25 @@
 /*jshint ignore:start*/
 var require = {
   scalejs: {
-      extensions: [
-        '<%=ext_name%>'
-      ]
+    extensions: [
+      '<%=ext_name%>'
+    ]
+  },
+  paths: {
+    requirejs: '../lib/requirejs/require',
+    scalejs: '../lib/scalejs/dist/scalejs.min',
+    'scalejs.mvvm.views': '../lib/scalejs.mvvm/dist/scalejs.mvvm',
+    knockout: '../lib/knockout/dist/knockout',
+    'scalejs.mvvm': '../lib/scalejs.mvvm/dist/scalejs.mvvm',
+    'knockout.mapping': '../lib/knockout.mapping/knockout.mapping',
+    'scalejs.functional': '../lib/scalejs.functional/dist/scalejs.functional.min',
+    text: '../lib/text/text'
+  },
+  packages: [
+
+  ],
+  shim: {
+
   }
 };
 /*jshint ignore:end*/
