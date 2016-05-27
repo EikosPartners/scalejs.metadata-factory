7756d7288d490e82abb4e873a51c60bd8650e8f8
Draisy <draisy.sabel@eikospartners.com>
2016-05-27 15:51:57 -0400

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,7 @@ module.exports = {
         alias: {
             // scalejs
             'scalejs.core': path.join(__dirname, 'node_modules/scalejs/dist/scalejs.core.js'),
-            'scalejs.sandbox': path.join(__dirname, 'node_modules/scalejs/dist/scalejs.sandbox.js'),
-
-            // extensions
-            'scalejs.extensions': path.join(__dirname, 'src/extensions/scalejs.extensions.js')
+            'scalejs.sandbox': path.join(__dirname, 'node_modules/scalejs/dist/scalejs.sandbox.js')
         }
     },
     output: {
