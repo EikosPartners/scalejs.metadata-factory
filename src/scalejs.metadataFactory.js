3b2ea73c6bea7c08c379a62942090c42126d5994
Draisy <draisy.sabel@eikospartners.com>
2016-05-31 16:29:27 -0400

    update extensions

--- src/scalejs.metadataFactory.js
+++ src/scalejs.metadataFactory.js
@@ -7,8 +7,8 @@ import 'scalejs.expression-jsep';
 import scalejs from 'scalejs';
 import core from 'scalejs.core';
 //var core = scalejs.core;
-import 'scalejs.mvvm';
-sandbox.mvvm.registerTemplates(view);
+//import 'scalejs.mvvm';
+core.mvvm.registerTemplates(view);
 
 var has = core.object.has,
     is = core.type.is,
