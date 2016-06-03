1a52dfafd093da2c912a7835c914538053693187
draisy <draisy@gmail.com>
2016-06-03 12:22:06 -0400

    update webpack configs

--- dist/extensions/scalejs.extensions.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-require('scalejs.mvvm');
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,11 @@ var path = require('path'),
     nodeExternals = require('webpack-node-externals');
 module.exports = {
     entry: "./src/scalejs.metadataFactory.js",
-    resolve: {
-        alias: {
-            // scalejs
-            // 'scalejs.core': path.join(__dirname, 'node_modules/scalejs/dist/scalejs.core.js'),
-            // 'scalejs.sandbox': path.join(__dirname, 'node_modules/scalejs/dist/scalejs.sandbox.js'),
-            // 'scalejs.extensions' : path.join(__dirname, 'src/extensions/scalejs.extensions.js')
-            
-        }
-    },
     output: {
         path: 'dist',
         filename: 'scalejs.metadataFactory.js'
     },
-    externals: [nodeExternals(), 'scalejs.core', 'scalejs.sandbox', 'scalejs.extensions'],
+    externals: [nodeExternals()],
     module: {
         loaders: [
             {
