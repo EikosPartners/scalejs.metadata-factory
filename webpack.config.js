ad1db1aa53be4f33950f77eda4ac09ec3ef319a8
draisy <draisy@gmail.com>
2016-06-03 09:43:56 -0400

    add resolve fallback

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,12 @@ module.exports = {
             // 'scalejs.core': path.join(__dirname, 'node_modules/scalejs/dist/scalejs.core.js'),
             // 'scalejs.sandbox': path.join(__dirname, 'node_modules/scalejs/dist/scalejs.sandbox.js'),
             // 'scalejs.extensions' : path.join(__dirname, 'src/extensions/scalejs.extensions.js')
-        }
+            
+        },
+        fallback: path.join(__dirname, "node_modules")
+    },       
+    resolveLoader: { 
+        fallback: path.join(__dirname, "node_modules") 
     },
     output: {
         path: 'dist',
